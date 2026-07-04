cdd954220ded2360ce0c5d87d8fb51ec72449e21
George Apostolov <george@livedemo.ai>
2026-07-03 22:58:06 -0400

    Add correct UrlDemo creation logic

--- src/models/UrlDemo.js
+++ src/models/UrlDemo.js
@@ -4,9 +4,9 @@ const UrlDemo = new mongoose.Schema(
     {
         url: { type: String, required: true },
         browserSessionId: { type: String },
-        type: { type: String, default: 'standard', enum: ['standard', 'browsed', 'owned'] },
+        type: { type: String, default: 'standard' }, // enum: ['standard', 'browsed', 'owned']
         storyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Story' },
-        status: { type: String, default: 'processing', enum: ['processing', 'completed'] },
+        status: { type: String, default: 'processing'}, //, enum: ['processing', 'completed'] 
         userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     },
     {
